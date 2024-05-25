82c908bee1c625f00d1bfca90c6e153e3b86b462
NHQHwng <nguyenhung.5703@gmail.com>
2024-05-25 14:50:50 +0700

    update response message for product list

--- FDiamondShopWebApp/Controllers/ProductController.cs
+++ FDiamondShopWebApp/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using FDiamondShop.API.Models.DTO;
 using FDiamondShop.API.Repository;
 using FDiamondShop.API.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace FDiamondShop.API.Controllers
 {
@@ -28,10 +29,20 @@ namespace FDiamondShop.API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<APIResponse>> GetAllProduct()
         {
-            IEnumerable<Product> ProductList = await _unitOfWork.ProductRepository.GetAllProductsAsync();
-            var model = _mapper.Map<List<ProductDTO>>(ProductList);
-            _response.Result = model;
-            return Ok(_response);
+            try
+            {
+                IEnumerable<Product> ProductList = await _unitOfWork.ProductRepository.GetAllProductsAsync();
+                var model = _mapper.Map<List<ProductDTO>>(ProductList);
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = model;
+                return Ok(_response);
+            }catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+                return BadRequest(_response);
+            }
         }
     }
 }
