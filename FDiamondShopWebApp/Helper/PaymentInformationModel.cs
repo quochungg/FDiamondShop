0b0bcedd00cc3a5465763c5c9606f7f6de0ca9fe
ArchieAsano <ArchieAsano@users.noreply.github.com>
2024-06-09 17:40:45 +0700

    MomoPay

--- FDiamondShopWebApp/Helper/PaymentInformationModel.cs
+++ FDiamondShopWebApp/Helper/PaymentInformationModel.cs
@@ -4,6 +4,7 @@
     {
         public string OrderType { get; set; }
         public double Amount { get; set; }
+        public string OrderID { get; set; }
         public string OrderDescription { get; set; }
         public string Name { get; set; }
     }
---
+++ FDiamondShopWebApp/Repository/IRepository/IMomoRepository.cs
@@ -0,0 +1,10 @@
+﻿using FDiamondShop.API.Helper;
+
+namespace FDiamondShop.API.Repository.IRepository
+{
+    public interface IMomoRepository
+    {
+        public  Task <Object> CreateMomoPaymentAsync(PaymentInformationModel model);
+
+    }
+}
