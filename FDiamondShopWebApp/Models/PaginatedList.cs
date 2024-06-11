2bad5500f49df099b359340917448d3288612e68
NHQHwng <nguyenhung.5703@gmail.com>
2024-06-11 17:10:14 +0700

    update pagination

---
+++ FDiamondShopWebApp/Models/PaginatedList.cs
@@ -0,0 +1,16 @@
+﻿namespace FDiamondShop.API.Models
+{
+    public class PaginatedList<T>
+    {
+        public int PageIndex { get; }
+        public int TotalProduct { get; }
+        public List<T> Items { get; }
+        public PaginatedList(List<T> items, int pageIndex, int totalProduct)
+        {
+            Items = items;
+            PageIndex = pageIndex;
+            TotalProduct = totalProduct;
+
+        }
+    }
+}
