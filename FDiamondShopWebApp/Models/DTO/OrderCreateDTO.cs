b008316804216e5c6ff8cbd75cfe02ea76c0b17a
Hà Duy Tùng <166358537+Tunghdse173573@users.noreply.github.com>
2024-06-15 02:42:43 +0200

    Delete FDiamondShopWebApp directory

--- FDiamondShopWebApp/Models/DTO/OrderCreateDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FDiamondShop.API.Models.DTO
-{
-    public class OrderCreateDTO
-    {  
-        public string? DiscountName { get; set; }
-        public string PaymentMethod { get; set; }
-
-    }
-}
