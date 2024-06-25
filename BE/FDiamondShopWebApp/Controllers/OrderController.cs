d7988fd195d8632c6532c4839cbc2e0461e50b0e
Tunghdse173573 <166358537+Tunghdse173573@users.noreply.github.com>
2024-06-25 15:21:25 +0700

    edit link

--- BE/FDiamondShopWebApp/Controllers/OrderController.cs
+++ BE/FDiamondShopWebApp/Controllers/OrderController.cs
@@ -99,7 +99,7 @@ namespace FDiamondShop.API.Controllers
                    
                     case "vnpay":
                         //sua link tren swagger
-                        var paymentApiUrl = new Uri(new Uri("https://localhost:7074/swagger"), "/api/checkout/vnpay");
+                        var paymentApiUrl = new Uri(new Uri("https://fdiamond-api.azurewebsites.net"), "/api/checkout/vnpay");
                         var paymentResponse = await _httpClient.PostAsJsonAsync(paymentApiUrl, paymentInfo);
                         if (paymentResponse.IsSuccessStatusCode)
                         {
@@ -127,7 +127,7 @@ namespace FDiamondShop.API.Controllers
                     case "momo":
                         decimal amountVND = await _unitOfWork.ExchangeRepository.ExchangeMoneyToVND(order.TotalPrice, "USD");
                         paymentInfo.Amount = (int)amountVND;
-                        var paymentApiUrlMomo = new Uri(new Uri("https://localhost:7074/swagger/index.html"), "/api/checkout/momo");
+                        var paymentApiUrlMomo = new Uri(new Uri("https://fdiamond-api.azurewebsites.net"), "/api/checkout/momo");
                         var paymentResponseMomo = await _httpClient.PostAsJsonAsync(paymentApiUrlMomo, paymentInfo);
 
                         if (paymentResponseMomo.IsSuccessStatusCode)
@@ -156,7 +156,7 @@ namespace FDiamondShop.API.Controllers
                     case "paypal":
                          
                         
-                        var paymentApiUrlPaypal = new Uri(new Uri("https://localhost:7074/swagger/index.html"), "/api/checkout/PayPal");
+                        var paymentApiUrlPaypal = new Uri(new Uri("https://fdiamond-api.azurewebsites.net"), "/api/checkout/PayPal");
                         var paymentResponsePaypal = await _httpClient.PostAsJsonAsync(paymentApiUrlPaypal, paymentInfo);
 
                         if (paymentResponsePaypal.IsSuccessStatusCode)
