ac8c3905ccbf69509eadd508da3d28e5c91b838b
DDTBao263 <92386788+DDTBao263@users.noreply.github.com>
2024-06-25 18:10:12 +0700

    commit again

--- src/components/upload/index.js
+++
@@ -1,2 +0,0 @@
-export { default as AddImageForm } from './AddImageForm';
-export { default as AddMutipleFile } from './AddMutipleFile';
--- src/sections/voucher/view/index.js
+++
@@ -1,1 +0,0 @@
-export { default as VoucherView } from './voucher-view';
