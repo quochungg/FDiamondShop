4270e0b39f7d7bdb0f2efdf6437f32ae32e5f20c
NHQHwng <nguyenhung.5703@gmail.com>
2024-07-01 15:28:44 +0700

    done update view cart

--- BE/FDiamondShopWebApp/Repository/DiscountCodeRepository.cs
+++ BE/FDiamondShopWebApp/Repository/DiscountCodeRepository.cs
@@ -12,6 +12,29 @@ namespace FDiamondShop.API.Repository
         {
             _db = db;
         }
-      
+        public DiscountCode FindinOrder(OrderCreateDTO createDTO)
+        {
+            return _db.DiscountCodes.SingleOrDefault (dc=>dc.DiscountCodeName == createDTO.DiscountName);
+        }
+        public DiscountCode CheckDuplicate(DiscountCodeCreateDTO discountCodeCreateDTO)
+        {
+            return _db.DiscountCodes.SingleOrDefault(dc => dc.DiscountCodeName == discountCodeCreateDTO.DiscountCodeName);
+
+        }
+        public async Task<DiscountReturnDTO> ApplyDiscount(ApplyDiscountDTO applyDiscountDTO)
+        {
+            var discountCode = await _db.DiscountCodes.FirstOrDefaultAsync(x => x.DiscountCodeName == applyDiscountDTO.DiscountCode) ?? throw new Exception("Discount code is invalid");
+            if (discountCode.IsExpried)
+            {
+                throw new Exception("Discount code is expried");
+            }
+            DiscountReturnDTO returnDTO = new()
+            {
+                ReduceAmount = applyDiscountDTO.Amount * discountCode.DiscountPercent / 100,
+                ReducePercent = discountCode.DiscountPercent
+            };
+            return returnDTO;
+
+        }
     }
 }
