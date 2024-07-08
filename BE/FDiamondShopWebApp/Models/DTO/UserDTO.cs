52beb93b76acc4ae6fccbf18c85732d6d709a8b3
NHQHwng <nguyenhung.5703@gmail.com>
2024-07-08 08:14:51 +0700

    add role to user detail

--- BE/FDiamondShopWebApp/Models/DTO/UserDTO.cs
+++ BE/FDiamondShopWebApp/Models/DTO/UserDTO.cs
@@ -8,8 +8,7 @@
         public string LastName { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
-        public bool IsGoogleAccount { get; set; } = false;
-        
-        
+        public bool IsGoogleAccount { get; set; } = false;       
+        public string Role { get; set; }
     }
 }
