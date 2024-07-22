a7783333feba993155ed6db3d47410bc0348096e
DuyenNguyen <duyenntbse183629@fpt.edu.vn>
2024-07-23 05:47:24 +0700

    last commit for footer

--- FE/fdiamond-customer/src/components/Footer.jsx
+++ FE/fdiamond-customer/src/components/Footer.jsx
@@ -4,7 +4,10 @@
 
 import { Link } from 'react-router-dom';
 import { FaInstagram } from "react-icons/fa";
-import { CiInstagram } from "react-icons/ci";
+import { RiFacebookFill } from "react-icons/ri";
+import { FaTwitter } from "react-icons/fa";
+import { IoLogoTiktok } from "react-icons/io5";
+import { FaSnapchatGhost } from "react-icons/fa";
 
 const Footer = () => {
 
@@ -12,15 +15,15 @@ const Footer = () => {
 
     return (
         <>
-            <div className='font-gantari'>
+            <div className='font-gantari border-t-[1px] border-gray-400 h-auto px-16'>
                 {/* <div className="flex items-center justify-center h-72 border-t-[1px] bg-[#000035] text-white"> */}
-                <div className='py-3'>
+                <div className='py-10'>
 
-                    <ul className='flex flex-row'>
-                        <li className='flex flex-col'>
+                    <ul className='flex flex-row justify-center items-start'>
+                        <li className='flex flex-col capitalize mr-40 font-[350] space-y-3 '>
                             <Link to={noContentPath}
                             >
-                                <p className='font-semibold'>Customer Care</p>
+                                <p className='font-semibold text-lg uppercase mb-1'>Customer Care</p>
                             </Link>
 
                             <Link to='tel:1800545457'>
@@ -42,10 +45,10 @@ const Footer = () => {
 
 
 
-                        <li className='flex flex-col'>
+                        <li className='flex flex-col capitalize mr-40 font-[350] space-y-3 '>
                             <Link to={noContentPath}
                             >
-                                <p className='font-semibold'>About FDIAMOND</p>
+                                <p className='font-semibold text-lg uppercase mb-1'>About FDIAMOND</p>
                             </Link>
 
                             <Link to={noContentPath}>Quality & Value</Link>
@@ -58,10 +61,10 @@ const Footer = () => {
 
 
 
-                        <li className='flex flex-col capitalize'>
+                        <li className='flex flex-col capitalize mr-40 font-[350] space-y-3 '>
                             <Link to={noContentPath}
                             >
-                                <p className='font-semibold'>Legal Area</p>
+                                <p className='font-semibold text-lg uppercase mb-1'>Legal Area</p>
                             </Link>
 
                             <Link to={noContentPath}>terms of use</Link>
@@ -73,20 +76,20 @@ const Footer = () => {
                         </li>
 
 
-                        <li className='flex flex-col capitalize'>
+                        <li className='flex flex-col capitalize  font-[350] space-y-3 '>
                             <Link to={noContentPath}
                             >
-                                <p className='font-semibold'>Follow Us</p>
+                                <p className='font-semibold text-lg uppercase mb-1'>Follow Us</p>
                             </Link>
 
-                            <div className='flex flex-row'>
-                                <Link to={noContentPath}>terms of use</Link>
-                                <Link to={noContentPath}>privacy policy</Link>
-                                <Link to={noContentPath}>conditions of sale</Link>
-                                <Link to={noContentPath}>credits</Link>
-                                <Link to={noContentPath}>accessibility statement</Link>
-                                <Link to={noContentPath}>Career</Link>
+                            <div className='flex flex-row space-x-10'>
+                                <Link to={noContentPath}><FaInstagram size={27} /></Link>
+                                <Link to={noContentPath}><RiFacebookFill size={27} /></Link>
+                                <Link to={noContentPath}><FaTwitter size={27} /></Link>
+                                <Link to={noContentPath}><IoLogoTiktok size={27} /></Link>
+                                <Link to={noContentPath}><FaSnapchatGhost size={27} /></Link>
                             </div>
+
 
                         </li>
                     </ul>
@@ -94,8 +97,7 @@ const Footer = () => {
                 </div>
 
                 <div>
-
-
+                    <p className='text-center text-[#888888] font-[350] text-sm py-5 border-t-[1px] border-gray-300'>© 2024 FDIAMOND. All Rights Reserved</p>
                 </div>
             </div>
         </>
