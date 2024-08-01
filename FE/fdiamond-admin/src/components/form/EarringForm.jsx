7d33adadbb53381b98a5c27617ffb720cf9280d5
DDTBao263 <92386788+DDTBao263@users.noreply.github.com>
2024-08-01 21:55:58 +0700

    Dropdown list

--- FE/fdiamond-admin/src/components/form/EarringForm.jsx
+++ FE/fdiamond-admin/src/components/form/EarringForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Grid, Select, MenuItem, InputLabel, FormControl, OutlinedInput } from '@mui/material';
+import { Grid, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
 
 function EarringForm({ formData, handleInputChange }) {
   return (
@@ -25,12 +25,26 @@ function EarringForm({ formData, handleInputChange }) {
       <Grid item md={4}>
         <FormControl fullWidth required>
           <InputLabel>Metal</InputLabel>
-          <OutlinedInput
+          {/* <OutlinedInput
             value={formData.EarringMetal}
             onChange={handleInputChange}
             label="Metal"
             name="EarringMetal"
-          />
+          /> */}
+          <Select
+            value={formData.EarringMetal}
+            onChange={handleInputChange}
+            label="Metal"
+            name="EarringMetal"
+          >
+            <MenuItem value="14k White Gold">14k White Gold</MenuItem>
+            <MenuItem value="18k White Gold">18k White Gold</MenuItem>
+            <MenuItem value="14k Yellow Gold">14k Yellow Gold</MenuItem>
+            <MenuItem value="18k Yellow Gold">18k Yellow Gold</MenuItem>
+            <MenuItem value="14k Rose Gold">14k Rose Gold</MenuItem>
+            <MenuItem value="18k Rose Gold">18k Rose Gold</MenuItem>
+            <MenuItem value="Platinum">Platinum</MenuItem>
+          </Select>
         </FormControl>
       </Grid>
     </>
